101cdb6b04f6a0dc57e8ae54fbb482f1d9703e1e
Leonardo Maciel <leonardoboeira.deisng@gmail.com>
2026-01-23 11:47:52 -0300

    Atualização das Chaves para BASE64

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,12 +1,15 @@
 import admin from "firebase-admin";
 
 if (!admin.apps.length) {
+  const serviceAccount = JSON.parse(
+    Buffer.from(
+      process.env.FIREBASE_SERVICE_ACCOUNT_BASE64 as string,
+      "base64"
+    ).toString("utf-8")
+  );
+
   admin.initializeApp({
-    credential: admin.credential.cert({
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
-    }),
+    credential: admin.credential.cert(serviceAccount),
   });
 }
 
